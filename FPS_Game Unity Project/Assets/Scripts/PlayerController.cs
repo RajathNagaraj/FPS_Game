88456a4e3af5c344409ff8603b3e2716fb6aef1f
Rajath Nagaraj <63661481+RajathNagaraj@users.noreply.github.com>
2022-10-04 13:22:04 +0530

    Made changes to PlayerController and PlayerMotor
    Implemented turning around and moving camera up and down

--- FPS_Game Unity Project/Assets/Scripts/PlayerController.cs
+++ FPS_Game Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,19 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
     private float speed;
+
+    private float lookSensitivity;
+
+    
+
 
     private PlayerMotor motor;
 
     private void Start()
     {
         speed = 5f;
+        lookSensitivity = 5f;
         motor = GetComponent<PlayerMotor>();
     }
     private void Update()
@@ -26,9 +32,35 @@ public class PlayerController : MonoBehaviour
         Vector3 _movVert = transform.forward * _zMove;
         //Final velocity for movement
         Vector3 _velocity = (_movHoriz + _movVert).normalized * speed;
-        //Applying velocity to motor
+
+        //Applying velocity to motor component
         motor.Move(_velocity);
 
+        //Calculate rotation as a 3D Vector (turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");        
+        Vector3 _rotation = new Vector3(0f ,_yRot , 0f);
+        //Applying sensitivity
+        _rotation *= lookSensitivity;
+
+        //Applying rotation to Player
+        motor.Rotate(_rotation);
+
+        //Mapping looking up and down to the camera
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        //We add a minus sign to xRot to invert axis
+        Vector3 _cameraRotation = new Vector3(-_xRot, 0f, 0f);
+
+        //Rotate the camera with look sensitivity
+        motor.RotateCamera(_cameraRotation * lookSensitivity);
+
+
         
+
+
+
+
     }
+
+    
 }
--- FPS_Game Unity Project/Assets/Scripts/PlayerMotor.cs
+++ FPS_Game Unity Project/Assets/Scripts/PlayerMotor.cs
@@ -7,6 +7,12 @@ using UnityEngine;
 public class PlayerMotor : MonoBehaviour
 {
 
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 cameraRotation = Vector3.zero;
+
+    [SerializeField] private Camera cam;
+
     Rigidbody rb;
 
     private void Start()
@@ -14,9 +20,70 @@ public class PlayerMotor : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    
 
-    public void Move(Vector3 velocity)
+
+    private void FixedUpdate()
     {
+        //Performs calculations on rigidbody in FixedUpdate
+        PerformMovement();
+        PerformRotation();
         
     }
+
+    //Perform camera rotation in update 
+    private void Update()
+    { 
+        PerformCameraRotation();
+    }
+
+
+    //Gets rotational Vector
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    //Apply rotational Vector on rigidbody
+    private void PerformRotation()
+    {
+        if(rotation != Vector3.zero)
+        {
+            rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        }
+
+    }
+
+
+    //Gets movement Vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //Apply movement Vector on rigidbody
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    //Apply rotational Vector on camera
+    private void PerformCameraRotation()
+    {
+        if (cameraRotation != Vector3.zero && cam != null)
+        {
+            cam.transform.Rotate(cameraRotation);
+        }
+
+    }
+
+
 }
